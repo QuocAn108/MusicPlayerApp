4e805cb6bd7058d4a2ce6c5c3432182b533ed875
Ryan <anhoang800140@gmail.com>
2024-11-12 23:44:13 +0700

    Add Song successfully

---
+++ MB.DAL/Repository/SongRepositories.cs
@@ -0,0 +1,56 @@
+﻿using MB.DAL.Data;
+using MB.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB.DAL.Repository
+{
+    public class SongRepositories
+    {
+        private MusicPlayerDBContext _context;
+        public List<Songs> GetAll()
+        {
+            _context = new();
+            return _context.Songs.ToList();
+        }
+        public Songs? GetById(int id)
+        {
+            _context = new();
+            return _context.Songs.FirstOrDefault(x => x.SongId == id);
+        }
+        public List<Songs> Search(string searchTerm)
+        {
+            return _context.Songs
+                .Where(s => s.Title.Contains(searchTerm) ||
+                           s.Artist.Contains(searchTerm) ||
+                           s.Album.Contains(searchTerm))
+                .ToList();
+        }
+
+        public void Add(Songs song)
+        {
+            _context = new();
+            song.DateAdded = DateTime.Now;
+            _context.Songs.Add(song);
+            _context.SaveChanges();
+        }
+
+        public void Update(Songs song)
+        {
+            _context = new();
+            _context.Songs.Update(song);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Songs song)
+        {
+            _context = new();
+            _context.Remove(song);
+            _context.SaveChanges();
+        }
+    }
+}
--- MusicPlayer/AddSongPage.xaml.cs
+++ MusicPlayer/AddSongPage.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using MB.DAL.Models;
+using Microsoft.Win32;
+using MP.BLL.Service;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,61 @@ namespace MusicPlayer
     /// </summary>
     public partial class AddSongPage : Page
     {
+        private readonly SongServices _songService =new();
+        private readonly OpenFileDialog _openFileDialog;
+
         public AddSongPage()
         {
             InitializeComponent();
+            _openFileDialog = new OpenFileDialog
+            {
+                Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*"
+            };
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_openFileDialog.ShowDialog() == true)
+            {
+                FilePathTextBox.Text = _openFileDialog.FileName;
+
+                // Tự động lấy thông tin từ file mp3 (nếu có)
+                var file = TagLib.File.Create(_openFileDialog.FileName);
+                TitleTextBox.Text = file.Tag.Title;
+                ArtistTextBox.Text = file.Tag.FirstPerformer;
+                AlbumTextBox.Text = file.Tag.Album;
+                DurationTextBox.Text = file.Properties.Duration.ToString(@"mm\:ss");
+            }
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                var song = new Songs
+                {
+                    Title = TitleTextBox.Text,
+                    Artist = ArtistTextBox.Text,
+                    Album = AlbumTextBox.Text,
+                    Duration = DurationTextBox.Text,
+                    FilePath = FilePathTextBox.Text
+                };
+
+                _songService.AddSong(song);
+                MessageBox.Show("Song added successfully!");
+
+                // Clear form
+                TitleTextBox.Clear();
+                ArtistTextBox.Clear();
+                AlbumTextBox.Clear();
+                DurationTextBox.Clear();
+                FilePathTextBox.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error adding song: {ex.Message}");
+            }
         }
     }
 }
