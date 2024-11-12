cb08ed721849df953092d2e69bf9c07dc03d84d0
Ryan <anhoang800140@gmail.com>
2024-11-12 18:29:57 +0700

    Init project and design layout app

---
+++ MB.DAL/Data/MusicPlayerDBContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB.DAL.Data
+{
+    public class MusicPlayerDBContext : dbcon
+    {
+        public DbSet<SongEntity> Songs { get; set; }
+        public DbSet<PlaylistEntity> Playlists { get; set; }
+
+        public MusicPlayerDbContext(DbContextOptions<MusicPlayerDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SongEntity>()
+                .HasIndex(s => s.FilePath)
+                .IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MB.DAL/Models/Songs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB.DAL.Models
+{
+    public class Songs
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+        [MaxLength(255)]
+        public string Artist { get; set; }
+
+        [MaxLength(255)]
+        public string Album { get; set; }
+
+        [Required]
+        public string Duration { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
+        public string FilePath { get; set; }
+
+        [MaxLength(100)]
+        public string Genre { get; set; }
+
+        public uint? Year { get; set; }
+
+        public byte[] AlbumArtData { get; set; }
+
+        [MaxLength(50)]
+        public string FileFormat { get; set; }
+
+        public long FileSize { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public DateTime? LastPlayed { get; set; }
+
+        public int PlayCount { get; set; }
+    }
+}
